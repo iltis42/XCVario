4a532e5e388e482eaafe3b99d3c71a4b7848c4d0
Eckhard Völlm <e.voellm@mail.de>
2020-07-21 17:28:44 +0200

    new version: sensor-20.0721-17.bin; Changes: Polar for SF27 and option to display battery in voltage

--- release.sh
+++ release.sh
@@ -1,8 +1,10 @@
 VER=`date +"%y.%m%d-%H"`
+comment=$1
 cd /home/nextpcb/sensor
 touch ./main/Version.h
 idf.py build
 cp ./build/sensor.bin  ./images/sensor-${VER}.bin
 git add ./images/sensor-${VER}.bin
-git commit -a -m "add new released sensor board binary: sensor-${VER}.bin"
+git commit -a -m "new version: sensor-${VER}.bin; Changes: $comment"
 git push
+git tag -a v${VER} -m $comment

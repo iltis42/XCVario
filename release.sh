37096ffce1bb758b68bb3d668e4725da75033443
Eckhard Völlm <e.voellm@mail.de>
2020-07-22 20:12:58 +0200

    new version: sensor-20.0722-20.bin; Changes: Polar update for SF27M

--- release.sh
+++ release.sh
@@ -7,4 +7,4 @@ cp ./build/sensor.bin  ./images/sensor-${VER}.bin
 git add ./images/sensor-${VER}.bin
 git commit -a -m "new version: sensor-${VER}.bin; Changes: $comment"
 git push
-git tag -a v${VER} -m $comment
+git tag -a v${VER} -m "$comment"

1efe56c9fb036c3b44fa58d0bed564d82a81a9cf
Eckhard Völlm <e.voellm@mail.de>
2020-08-30 23:41:13 +0200

    add logdef.h and switch to cpp mode win onewire_rmt.cpp implementation

---
+++ main/logdef.h
@@ -0,0 +1,13 @@
+#ifndef LOGDEF_H
+#define LOGDEF_H
+
+#include <string>
+#include "esp_log.h"
+#include <string.h>
+
+#define S1(x) #x
+#define S2(x) S1(x)
+#define FNAME basename(__FILE__)
+
+
+#endif

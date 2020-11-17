8401cd223cc7b5d84da789dd152cc12a8684613a
Eckhard Völlm <e.voellm@mail.de>
2020-11-17 22:57:25 +0100

    remove ttyS2 as option for serial loop with hardware revsision <3

--- main/SetupMenu.cpp
+++ main/SetupMenu.cpp
@@ -933,21 +933,22 @@ void SetupMenu::setup( )
 		s2sp->addEntry( "57600 baud");
 		s2sp->addEntry( "115200 baud");
 
-		SetupMenuSelect * s1in = new SetupMenuSelect( PROGMEM "RX Routing", 0, false, 0, true, &serial1_rxloop );
+		SetupMenuSelect * s1in = new SetupMenuSelect( PROGMEM "Serial Loops", 0, false, 0, true, &serial1_rxloop );
 		rs232->addMenu( s1in );
-		s1in->setHelp( "Option to route ttyS1 RX, usually from FLARM to serial TX port for OV or Kobo" );
-		s1in->addEntry( "Disable");       // 0
-		s1in->addEntry( "Enable ttyS1");  // 1
-		s1in->addEntry( "Enable ttyS2");  // 2
+		s1in->setHelp( "Option to loop serial RX to ttyS1 TX port, e.g. for unidirectional OV or Kobo connection" );
+		s1in->addEntry( "Disable");          // 0
+		s1in->addEntry( "Enable ttyS1 RX");  // 1
+		if( hardwareRevision >= 3 )
+			s1in->addEntry( "Enable ttyS2 RX");  // 2
 
 
-		SetupMenuSelect * s1out = new SetupMenuSelect( PROGMEM "TX Routing", 0, false, 0, true, &serial1_tx );
+		SetupMenuSelect * s1out = new SetupMenuSelect( PROGMEM "Serial Routing", 0, false, 0, true, &serial1_tx );
 		rs232->addMenu( s1out );
-		s1out->setHelp( "Select source of data for sending on serial interface ttyS1 TX (usually going to FLARM)");
+		s1out->setHelp( "Select devices routed to serial interface ttyS1");
 		s1out->addEntry( "Disable all");
-		s1out->addEntry( "XCVario");                     // 1    XCVario NMEA Data
-		s1out->addEntry( "Bluetooth Dev");               // 2    XCSoar Data
-		s1out->addEntry( "Bluetooth Dev, XCVario");      // 3
+		s1out->addEntry( "XCVario");                 // 1    XCVario NMEA Data
+		s1out->addEntry( "Bluetooth-XCSoar");      // 2    XCSoar Data
+		s1out->addEntry( "BT-XCSoar, XCVario");      // 3
 
 
 		SetupMenuSelect * stxi = new SetupMenuSelect( PROGMEM "Serial TX Inversion", 0, true , 0, true, &serial1_tx_inverted );
@@ -978,11 +979,11 @@ void SetupMenu::setup( )
 
 			SetupMenuSelect * s1out2 = new SetupMenuSelect( PROGMEM "Routing", 0, false, 0, true, &serial2_tx );
 			rs232_2->addMenu( s1out2 );
-			s1out2->setHelp( "Select source of data for exchange with serial interface ttyS2 (usually going to a device)");
+			s1out2->setHelp( "Select devices routed to serial interface ttyS2");
 			s1out2->addEntry( "Disable all");
 			s1out2->addEntry( "XCVario");                     // 1    XCVario NMEA Data bidir
-			s1out2->addEntry( "Bluetooth Dev");               // 2    XCSoar Data bidir
-			s1out2->addEntry( "Bluetooth Dev, XCVario");      // 3
+			s1out2->addEntry( "Bluetooth-XCSoar");               // 2    XCSoar Data bidir
+			s1out2->addEntry( "BT-XCSoar, XCVario");      // 3
 
 			SetupMenuSelect * stxi2 = new SetupMenuSelect( PROGMEM "Serial TX Inversion", 0, true , 0, true, &serial2_tx_inverted );
 			rs232_2->addMenu( stxi2 );

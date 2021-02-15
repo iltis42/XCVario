fcee4e6db1e48721c229a2b23711e41b456cbe12
Eckhard Völlm <e.voellm@mail.de>
2021-02-15 16:28:24 +0100

    added flap label 3a, 3b e.g. for Schleicher ASW27

--- main/Flap.cpp
+++ main/Flap.cpp
@@ -227,18 +227,26 @@ void Flap::setupMenue( SetupMenu *parent ){
 	flab0->addEntry( " N" );   // N,L,S
 	flab0->addEntry( " L" );
 	flab0->addEntry( " S" );
+	flab0->addEntry( " 3a" );
+	flab0->addEntry( " 3b" );
 
 	flabp1->addEntry( " N" );
 	flabp1->addEntry( " L" );
 	flabp1->addEntry( " S" );
+	flabp1->addEntry( " 3a" );
+	flabp1->addEntry( " 3b" );
 
 	flabp2->addEntry( " N" );
 	flabp2->addEntry( " L" );
 	flabp2->addEntry( " S" );
+	flabp2->addEntry( " 3a" );
+	flabp2->addEntry( " 3b" );
 
 	flabp3->addEntry( " N" );
 	flabp3->addEntry( " L" );
 	flabp3->addEntry( " S" );
+	flabp3->addEntry( " 3a" );
+	flabp3->addEntry( " 3b" );
 
 	flabm1->addEntry( " N" );
 	flabm1->addEntry( " L" );
@@ -251,6 +259,7 @@ void Flap::setupMenue( SetupMenu *parent ){
 	flabm3->addEntry( " N" );
 	flabm3->addEntry( " L" );
 	flabm3->addEntry( " S" );
+
 	flapLabels[0] = flabm3;
 	flapLabels[1] = flabm2;
 	flapLabels[2] = flabm1;

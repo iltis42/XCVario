458c5fb96b750cb7a2f7ab90a8819a751494faf6
jlder <jeanluc@derouineau.com>
2021-04-23 21:58:04 +0200

    Add GNSS UBX frame detection and decoding. Add Vsz_gps and Ground_Speed_gps to XCVario_Dev NMEA sentence

--- main/Router.cpp
+++ main/Router.cpp
@@ -100,17 +100,9 @@ void Router::routeS1(){
 	//modif gfm
 	int i;
 	if( pullMsg( s1_rx_q, s1) ){
-	  if( !strcmp(s1.c_str(),"µb")){//Si le message reçu est un message GPS UBX
-		// on l'envoie sur le BT
-		if( blue_enable.get() == WL_WLAN )
-			if( forwardMsg( s1, wl_flarm_tx_q ))
-				ESP_LOGV(FNAME,"ttyS1 RX bytes %d forward to wl_flarm_tx_q port 8881", s1.length() );
-		if( blue_enable.get() == WL_BLUETOOTH )
-			if( forwardMsg( s1, bt_tx_q ))
-				ESP_LOGV(FNAME,"ttyS1 RX bytes %d forward to bt_tx_q", s1.length() );
-        // puis il est décodé
-		for(i=0;i<s1.length();i++) parse(s1.c_str()[i]);
-	  }
+	  if( !strcmp(s1.c_str(),"µb")){ // if message is from GNSS in UBX format
+	    for(i=0;i<s1.length();i++) parse(s1.c_str()[i]); // decode it
+	  } else {
 		ESP_LOGD(FNAME,"ttyS1 RX len: %d bytes, Q:%d", s1.length(), bt_tx_q.isFull() );
 		ESP_LOG_BUFFER_HEXDUMP(FNAME,s1.c_str(),s1.length(), ESP_LOG_DEBUG);
 		Protocols::parseNMEA( s1.c_str() );
@@ -126,6 +118,7 @@ void Router::routeS1(){
 		if( serial2_tx.get() & RT_S1 )
 			if( forwardMsg( s1, s2_tx_q ))
 				ESP_LOGV(FNAME,"ttyS1 RX bytes %d looped to s2_tx_q", s1.length() );
+	  }
 	}
 }
 

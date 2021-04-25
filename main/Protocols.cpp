1982173314c77ba5b44c67eba50bcf1a7ae164fd
jlder <jeanluc@derouineau.com>
2021-04-25 23:43:46 +0200

    increase XCV sentence pressure data

--- main/Protocols.cpp
+++ main/Protocols.cpp
@@ -138,7 +138,7 @@ void Protocols::sendNMEA( proto_t proto, char* str, float baro, float dp, float 
 	if( proto == P_XCVARIO_DEVEL ){
 		float roll = IMU::getRoll();
 		float pitch = IMU::getPitch();
-		sprintf(str,"$PXCV,%3.1f,%1.1f,%d,%1.2f,%d,%2.1f,%4.1f,%4.1f,%4.1f,%3.1f,%3.1f,%1.4f,%1.4f,%1.4f", te, Units::Vario2ms(mc), bugs, (aballast+100)/100.0, cruise, temp, QNH.get(), baro, dp, roll, pitch, acc_x, acc_y, acc_z );
+		sprintf(str,"$PXCV,%3.1f,%1.1f,%d,%1.2f,%d,%2.1f,%.3f,%.3f,%.3f,%3.1f,%3.1f,%1.4f,%1.4f,%1.4f", te, Units::Vario2ms(mc), bugs, (aballast+100)/100.0, cruise, temp, QNH.get(), baro, dp, roll, pitch, acc_x, acc_y, acc_z );
 	}
 	else if( proto == P_XCVARIO ){
 		/*

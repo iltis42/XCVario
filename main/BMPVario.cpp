334ec156c831113552fbdaea21c2e6d72936216e
Eckhard Völlm <e.voellm@mail.de>
2021-07-04 21:56:00 +0200

    fix latency supervision in TE sensor task that may potentially hangup vario reading

--- main/BMPVario.cpp
+++ main/BMPVario.cpp
@@ -62,15 +62,14 @@ double BMPVario::readTE( float tas ) {
 
 	uint64_t rts = esp_timer_get_time();
 	float delta = (float)(rts - lastrts)/1000000.0;   // in seconds
+	lastrts = rts;
 	// ESP_LOGI(FNAME, "TE time delta %4.3f", delta );
 	if( (delta < 0.075) || (delta > 0.400) ) { // ensure delta is 100 mS +-
-		// ESP_LOGI(FNAME, "Invalid TE time delta (<75 || >250) mS: %4.1f", delta*1000 );
+		ESP_LOGW(FNAME, "Invalid TE time delta (<75 || >400) mS: %4.1f", delta*1000 );
 		return _TEF;
 	}
 
 	// ESP_LOGI(FNAME,"Vario delta=%2.3f sec", delta );
-
-	lastrts = rts;
 	// ESP_LOGI(FNAME, "TE-Alt %0.1f  NM:", _currentAlt );
 	if( _init  ){
 		vTaskDelay(100 / portTICK_PERIOD_MS);

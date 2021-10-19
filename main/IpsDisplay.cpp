e555ab7556eab3cd62c77adcc6bb2fdd5694c3c0
Helmut J. Rohs <helmut.j.rohs@web.de>
2021-10-19 16:28:32 +0200

    Pu s2f mode icon to the top rigth icon area.

--- main/IpsDisplay.cpp
+++ main/IpsDisplay.cpp
@@ -1715,7 +1715,7 @@ void IpsDisplay::drawRetroDisplay( int airspeed_kmh, float te_ms, float ate_ms, 
 
 	// Cruise mode or circling
 	if( (int)s2fmode != s2fmode_prev ){
-		drawS2FMode( 17, 272, s2fmode );
+		drawS2FMode( DISPLAY_W-50, FLOGO-2, s2fmode );
 		s2fmode_prev = (int)s2fmode;
 	}
 

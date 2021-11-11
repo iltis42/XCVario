b39ce6765b45a9de921e6f2095cd3aa3fb3026f6
Andreas Titze <79600736+MoskitoAndi@users.noreply.github.com>
2021-11-11 07:49:04 +0100

    Update AdaptUGC.h

--- main/AdaptUGC.h
+++ main/AdaptUGC.h
@@ -24,6 +24,7 @@ public:
 	inline void drawRBox(int16_t x, int16_t y, int16_t w, int16_t h, int16_t r)  { eglib_DrawRoundBox(eglib, x, y, w, h, r); }
 	inline void drawRFrame(int16_t x, int16_t y, int16_t w, int16_t h, int16_t r)  { eglib_DrawRoundFrame(eglib, x, y, w, h, r); }
 	inline void drawTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2)  { eglib_DrawTriangle(eglib, x0, y0, x1, y1, x2, y2); }	
+	inline void drawTetragon(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, int16_t x3, int16_t y3)  { eglib_DrawTriangle(eglib, x0, y0, x1, y1, x2, y2); eglib_DrawTriangle(eglib, x0, y0, x2, y2, x3, y3); }	
 	
 	
 	void drawCircle(int16_t x, int16_t y, int16_t radius, uint8_t options){                                                                        // adapter
@@ -71,8 +72,6 @@ public:
 	
 	// two things done above, rest tbd:
 	/*
-	
-	ucg->drawTetragon
 
 	ucg->invertDisplay
 	ucg->print

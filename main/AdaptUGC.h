cea9747637da43f7566b90074930757db7d43019
Andreas Titze <79600736+MoskitoAndi@users.noreply.github.com>
2021-11-12 17:15:59 +0100

    Update AdaptUGC.h

--- main/AdaptUGC.h
+++ main/AdaptUGC.h
@@ -81,12 +81,13 @@ public:
 	size_t write(uint8_t c) { 
 	//  ucg_int_t delta;
 	//  delta = ucg_DrawGlyph(get_ucg(), get_tx(), get_ty(), get_tdir(), c); 
- 	switch(eglib_print_dir) {
-    		case 0: eglib_print_xpos += delta; break;
-    		case 1: eglib_print_ypos += delta; break;
-    		case 2: eglib_print_xpos -= delta; break;
-    		default: case 3: eglib_print_ypos -= delta; break;
-  	}
+ 		switch(eglib_print_dir) {
+    			case 0: eglib_print_xpos += delta; break;
+    			case 1: eglib_print_ypos += delta; break;
+    			case 2: eglib_print_xpos -= delta; break;
+    			default: case 3: eglib_print_ypos -= delta; break;
+  		}
+	}
 private:
 	int16_t eglib_print_xpos = 0, eglib_print_ypos = 0;
 	uint8_t eglib_print_dir = UCG_PRINT_DIR_LR;

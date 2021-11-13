eecce225dafa02ab4b3de78d8498912e5fcdd280
Andreas Titze <79600736+MoskitoAndi@users.noreply.github.com>
2021-11-13 16:04:28 +0100

    Update AdaptUGC.h

--- main/AdaptUGC.h
+++ main/AdaptUGC.h
@@ -79,8 +79,11 @@ public:
 		int8_t delta;
 	//  delta = ucg_DrawGlyph(get_ucg(), get_tx(), get_ty(), get_tdir(), c); 
 	// eglib_DrawGlyph(eglib_t * eglib, coordinate_t x, coordinate_t y, const struct glyph_t * glyph)
- 		eglib_DrawWChar(eglib, eglib_print_xpos, eglib_print_ypos, wchar_t (c) );
-		delta = eglib_GetTextWidth(eglib, &c);
+ 	//	eglib_DrawWChar(eglib, eglib_print_xpos, eglib_print_ypos, wchar_t (c) );
+		const struct glyph_t * g;
+		g = eglib_GetGlyph(eglib, wchar_t (c));
+		eglib_DrawGlyph(eglib, eglib_print_xpos, eglib_print_ypos, g)
+		delta = g.advance;
 		switch(eglib_print_dir) {
     			case 0: eglib_print_xpos += delta; break;
     			case 1: eglib_print_ypos += delta; break;

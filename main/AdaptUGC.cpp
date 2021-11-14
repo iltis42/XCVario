bd7fa0bf730402b040263c0738f754dac2ca0ad0
Eckhard Völlm <e.voellm@mail.de>
2021-11-14 21:45:18 +0100

    code cleanup in eglib layer

--- main/AdaptUGC.cpp
+++ main/AdaptUGC.cpp
@@ -9,7 +9,6 @@
 static eglib_t myeglib;
 
 void  AdaptUGC::begin() {
-	// eglib = ... tbd.
 	st7789_config_t st7789_config = {
 			.width = 240,
 			.height = 320,
@@ -22,20 +21,18 @@ void  AdaptUGC::begin() {
 	};
 
 	esp32_hal_config_t esp32_ili9341_config = {
-		.spi_num = 	HSPI,
-		.freq = 	13111111*3,  // max 40 MHz
-		.dataMode = SPI_MODE3,
-		.bitOrder = MSBFIRST,
-		.gpio_scl = SPI_SCLK,
-		.gpio_sda = SPI_MOSI,
-		.gpio_sdi = SPI_MISO,
-		.gpio_cs  = CS_Display,
-		.gpio_dc  = SPI_DC,
-		.gpio_rs  = RESET_Display,
+			.spi_num = 	HSPI,
+			.freq = 	13111111*3,  // max 40 MHz
+			.dataMode = SPI_MODE3,
+			.bitOrder = MSBFIRST,
+			.gpio_scl = SPI_SCLK,
+			.gpio_sda = SPI_MOSI,
+			.gpio_sdi = SPI_MISO,
+			.gpio_cs  = CS_Display,
+			.gpio_dc  = SPI_DC,
+			.gpio_rs  = RESET_Display,
 	};
-
 	eglib_Init( &myeglib, &esp32_ili9341, &esp32_ili9341_config, &st7789, &st7789_config );
-
 };
 
 

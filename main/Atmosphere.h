1e4f7d54888e1a4952b8f9f5bd5eecd35b1790fc
Eckhard Völlm <e.voellm@mail.de>
2021-11-24 22:14:20 +0100

    correction of kmh2pascal formula for second vario TAS display

--- main/Atmosphere.h
+++ main/Atmosphere.h
@@ -43,7 +43,7 @@ public:
 		return sqrt( 2*pascal / 1.225 )*3.6;
 	};
 	static float kmh2pascal( float kmh ){
-		return (kmh*kmh/3.6*3.6) * 1.225/2.;
+		return ((kmh/3.6)*(kmh/3.6)) * 1.225/2.;
 	};
 	static inline double calcAltitude(double SeaLevel_Pres, double pressure) {
 			return ( 44330.0 * (1.0 - pow(pressure / SeaLevel_Pres, (1.0/5.255))) );

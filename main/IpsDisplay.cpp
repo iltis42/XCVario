b6c2a5dd1e0f8dc09306108a0f3abab9131788c6
MoskitoAndi <Andreas.Titze@web.de>
2021-12-21 14:00:36 +0100

    Minor fix to void overlap between QnH and Unit line in Retro/UL screen.

--- main/IpsDisplay.cpp
+++ main/IpsDisplay.cpp
@@ -1411,7 +1411,7 @@ bool IpsDisplay::drawAltitude( float altitude, int16_t x, int16_t y, bool dirty,
 		// QNH
 		int16_t qnh_x = x+5+ucg->getStrWidth(s);
 		sprintf(s, "%d", Units::QnhRounded(QNH.get()));
-		ucg->setPrintPos(qnh_x - ucg->getStrWidth(s), y-17);
+		ucg->setPrintPos(qnh_x - ucg->getStrWidth(s), y-19);
 		ucg->setColor( COLOR_WHITE );
 		ucg->print(s);
 	}

465ffcac482872099398336bb878acfb1de17fd0
Eckhard Völlm <e.voellm@mail.de>
2022-07-29 12:13:24 +0200

    fix averager

--- main/AverageVario.h
+++ main/AverageVario.h
@@ -1,6 +1,10 @@
 #ifndef AVERAGE_VARIO_H
 #define AVERAGE_VARIO_H
 
+#include <list>
+#include <vector>
+#include <array>
+
 class AverageVario{
 	AverageVario() {};
 	~AverageVario() {};
@@ -13,12 +17,11 @@ public:
 private:
 	static float averageClimbSec;
 	static float averageClimb;
-	static float avClimb100MSec[10];
-	static float avClimbSec[60];
-	static float avClimbMin[300];
+	static std::array<float, 10> avClimb100MSec;
+	static std::array<float, 60> avClimbSec;
+	static std::list<float> avClimbMin;
 	static int avindex100MSec;
 	static int avindexSec;
-	static int avindexMin;
 	static int samples;
 };
 

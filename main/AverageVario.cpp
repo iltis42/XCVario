4e795ff0280b813b664512a5e333f277137439a3
Eckhard Völlm <e.voellm@mail.de>
2022-07-29 13:20:04 +0200

    by default resize list for average climb values

--- main/AverageVario.cpp
+++ main/AverageVario.cpp
@@ -22,6 +22,7 @@ void AverageVario::begin(){
 	averageClimbSec = 0.0;
 	avindexSec = 0;
 	avindex100MSec = 0;
+	avClimbMin.resize( core_climb_history.get() );
 }
 
 void AverageVario::recalcAvgClimb() {

395ac43e79cb1738b7791d826537f8a36251271c
Eckhard Völlm <e.voellm@mail.de>
2023-02-18 22:53:38 +0100

    added compilable BLE library module from arduiono-esp32

---
+++ main/BLESender.cpp
@@ -0,0 +1,103 @@
+#include <esp_log.h>
+#include "BLESender.h"
+#include <string>
+#include "sdkconfig.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_task_wdt.h"
+#include <freertos/semphr.h>
+#include <algorithm>
+#include "RingBufCPP.h"
+#include <driver/uart.h>
+#include "Protocols.h"
+#include <logdef.h>
+#include "Switch.h"
+#include "sensor.h"
+#include "Router.h"
+#include "Flarm.h"
+#include "BluetoothSerial.h"
+#include "DataMonitor.h"
+#include "DataLink.h"
+
+static TaskHandle_t pid = nullptr;
+
+DataLink *dlb;
+
+bool BLESender::selfTest(){
+	ESP_LOGI(FNAME,"SerialBT::selfTest");
+	if( !SerialBT ){
+		ESP_LOGI(FNAME,"SerialBT not initialized");
+		return false;
+	}
+	if( SerialBT->isReady( false ) ) {
+		ESP_LOGI(FNAME,"SerialBT::selfTest: PASSED");
+		return true;
+	}
+	ESP_LOGI(FNAME,"SerialBT::selfTest: FAILED");
+	return false;
+}
+
+BluetoothSerial *BLESender::SerialBT = 0;
+
+int BLESender::queueFull() {
+	if( wireless == WL_BLUETOOTH ){
+		if(bt_tx_q.isFull())
+			return 1;
+	}
+	return 0;
+}
+
+void BLESender::btTask(void *pvParameters){
+	while(1) {
+		progress();
+		Router::routeBT();
+		if( uxTaskGetStackHighWaterMark( pid ) < 256 )
+			ESP_LOGW(FNAME,"Warning BT task stack low: %d bytes", uxTaskGetStackHighWaterMark( pid ) );
+		vTaskDelay( 20/portTICK_PERIOD_MS );
+	}
+}
+
+void BLESender::progress(){
+	if (wireless != WL_BLUETOOTH )
+		return;
+	if( !SerialBT ){
+		ESP_LOGI(FNAME,"SerialBT not initialized");
+		return;
+	}
+	char buf[400];
+	int pos = 0;
+	while(SerialBT->available() && (pos < 256) ) {
+		char byte = (char)SerialBT->read();
+		buf[pos] = byte;
+		pos++;
+	}
+	if( pos ){
+		SString rx;
+		rx.set( buf, pos );
+		dlb->process( buf, pos, 7 );
+		DM.monitorString( MON_BLUETOOTH, DIR_RX, rx.c_str(), pos );
+		// ESP_LOGI(FNAME,">BT RX: %d bytes", pos );
+		// ESP_LOG_BUFFER_HEXDUMP(FNAME,rx.c_str(),pos, ESP_LOG_INFO);
+	}
+	if( SerialBT->hasClient() ) {
+		int len = Router::pullBlock( bt_tx_q, buf, 400 );
+		if( len ){
+			// ESP_LOGI(FNAME,"<BT TX %d bytes", msg.length() );
+			// ESP_LOG_BUFFER_HEXDUMP(FNAME,msg.c_str(),msg.length(), ESP_LOG_INFO);
+			SerialBT->write( (const uint8_t *)buf, len );
+			DM.monitorString( MON_BLUETOOTH, DIR_TX, buf, len );
+		}
+	}
+}
+
+void BLESender::begin(){
+	ESP_LOGI(FNAME,"BLESender::begin()" );
+	if( wireless == WL_BLUETOOTH ) {
+		ESP_LOGI(FNAME,"BT on, create BT master object" );
+		dlb = new DataLink();
+		SerialBT = new BluetoothSerial();
+		SerialBT->begin( SetupCommon::getID() );
+		xTaskCreatePinnedToCore(&btTask, "bleTask", 4096, NULL, 12, &pid, 0);  // stay below compass task
+	}
+}
---
+++ main/BLESender.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// #include <btstack.h>
+#include <esp_log.h>
+#include "RingBufCPP.h"
+#include <string>
+#include "BluetoothSerial.h"
+
+#define RFCOMM_SERVER_CHANNEL 1
+#define HEARTBEAT_PERIOD_MS 50
+#define SPP_SERVICE_BUFFER_SIZE 1024
+
+class BLESender {
+
+public:
+  BLESender() { SerialBT = 0; };
+  void begin();
+  static int queueFull();
+  static void btTask(void *pvParameters);
+  static void progress();  // progress loop
+  bool selfTest();         // call 3 seconds after begin
+
+private:
+
+   static BluetoothSerial *SerialBT;
+
+};
+

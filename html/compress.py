13f988858cc02dc643a4eb0046bf6a2ddd6aa07d
DeeFuse <7376678+DeeFuse@users.noreply.github.com>
2023-04-18 16:53:52 +0200

    Auto compress html assets with cmake build system - default to using compressed assets

--- html/compress.py
+++ html/compress.py
@@ -1,10 +1,12 @@
 import gzip
-import glob
 import shutil
+import sys
+import os
 
-extensions = ('*.html', '*.css') # files to compress
-for extension in extensions:
-    for file in glob.glob(extension):
-        with open(file, 'rb') as f_in:
-            with gzip.open(file + ".gz", 'wb', 9) as f_out:
-                shutil.copyfileobj(f_in, f_out)
+if __name__ == "__main__":
+    full_path = sys.argv[1]
+    file_name = os.path.basename(full_path)
+    print(f"Compressing: {file_name} > {file_name}.gz")
+    with open(full_path, 'rb') as f_in:
+        with gzip.open(full_path + ".gz", 'wb', 9) as f_out:
+            shutil.copyfileobj(f_in, f_out)

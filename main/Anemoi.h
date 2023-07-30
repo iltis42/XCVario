ad25f6b9b0a99925ccf359db122762a3bedbf71d
Helmut J. Rohs <helmut.j.rohs@web.de>
2023-07-31 01:04:19 +0200

    The anemoi wind parser.

---
+++ main/Anemoi.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+class Anemoi {
+public:
+	static void parseWind( const char *wind );
+	static void parseStatus( const char *status );
+//	static bool connected(); // returns true if Flarm is connected
+
+};
+

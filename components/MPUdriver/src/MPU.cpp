5de28aa275f182abb43cf6ef429cc489537b9f4f
Helmut J. Rohs <helmut.j.rohs@web.de>
2023-10-29 16:29:38 +0100

    Add missing initializer.

--- components/MPUdriver/src/MPU.cpp
+++ components/MPUdriver/src/MPU.cpp
@@ -176,7 +176,9 @@ void MPU::pwm_init(){
 			.channel = LEDC_CHANNEL_1,
 			.intr_type = LEDC_INTR_DISABLE,
 			.timer_sel = LEDC_TIMER_1,
-			.duty = 0, .hpoint = 0 };
+			.duty = 0,
+			.hpoint = 0,
+			.flags = {.output_invert = 0} };
 	ledc_channel_config(&pwm_ch);
 	ledc_timer_config(&pwm_timer);
 	temp_control( 0, 45.0 );

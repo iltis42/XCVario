702dedf1c271e451989e3f57e42d64774ea0f6be
Helmut J. Rohs <helmut.j.rohs@web.de>
2024-09-11 22:16:07 +0200

    Proposal of new DataLink being able to be extended with configurable protocols through the protocol interface. Constructed with a proper router target for the port.

--- main/DataLink.h
+++ main/DataLink.h
@@ -1,7 +1,11 @@
 #pragma once
-#include "SString.h"
+
+#include "protocols/ProtocolItf.h"
 #include "types.h"
 
+class SString;
+
+typedef void (*router_t)(SString &s2);
 
 /**
  *
@@ -39,23 +43,48 @@ enum state_t {
 
 
 class DataLink {
-	public:
-		DataLink();
-		void process( const char *packet, int len, int port );
+public:
+	DataLink();
+	void process( const char *packet, int len, int port );
 
-	private:
-		void parse_NMEA_UBX( char c, int port );
-		void processNMEA( char * buffer, int len, int port );
-		void addChk(const char c);
-		void routeSerialData( const char *data,uint32_t len, int port, bool nmea );
-		enum state_t state;
-		char framebuffer[128];
-		int  pos;
-		int  len;
-		char chkA; // checksum variables
-		char chkB;
-		char chkBuf;
-		bool nmeaFound;
-		bool ubxFound;
-		unsigned char krt2_len;
+private:
+	void parse_NMEA_UBX( char c, int port );
+	void processNMEA( char * buffer, int len, int port );
+	void addChk(const char c);
+	void routeSerialData( const char *data,uint32_t len, int port, bool nmea );
+	enum state_t state;
+	char framebuffer[128];
+	int  pos;
+	int  len;
+	char chkA; // checksum variables
+	char chkB;
+	char chkBuf;
+	bool nmeaFound;
+	bool ubxFound;
+	unsigned char krt2_len;
 };
+
+
+/**
+ *
+ * DataLink
+ * This layer handles all inbound unspecific data and precesses it byte per byte to a configured list of 
+ * protocol parser for this port.
+ *
+ */
+class DataLinkNT {
+public:
+	DataLinkNT(router_t route);
+	void setProtocol(protocol_nt ptyp);
+	void process(const char *packet, const int len);
+
+
+private:
+	// could be also a vector of protocols
+	ProtocolItf* _protocol = nullptr;
+	// the port's proper queue for the traffic routing
+	router_t _router;
+};
+
+extern DataLinkNT dl_S1;
+extern DataLinkNT dl_S2;

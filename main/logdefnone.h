54f16d20568d84cfbb037d81ccc7f03c5e4f00a1
Helmut J. Rohs <helmut.j.rohs@web.de>
2024-12-25 21:24:43 +0100

    Suppress logging optionally entirely.

---
+++ main/logdefnone.h
@@ -0,0 +1,7 @@
+
+#pragma once
+
+#define ESP_LOGD(l, fmt, ...) // Do nothing
+#define ESP_LOGI(l, fmt, ...) // Do nothing
+#define ESP_LOGW(l, fmt, ...) // Do nothing
+#define ESP_LOGE(l, fmt, ...) // Do nothing

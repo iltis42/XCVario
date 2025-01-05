c52feafa6534a793f6ad8b6eab50e59b6fe9a1d1
Helmut J. Rohs <helmut.j.rohs@web.de>
2025-01-05 19:20:58 +0100

    Add FlarmHost protocol to forward an proxy for the NAVI_DEV devices.

---
+++ main/protocol/FlarmHost.h
@@ -0,0 +1,27 @@
+/***********************************************************
+ ***   THIS DOCUMENT CONTAINS PROPRIETARY INFORMATION.   ***
+ ***    IT IS THE EXCLUSIVE CONFIDENTIAL PROPERTY OF     ***
+ ***     Rohs Engineering Design AND ITS AFFILIATES.     ***
+ ***                                                     ***
+ ***       Copyright (C) Rohs Engineering Design         ***
+ ***********************************************************/
+
+#pragma once
+
+#include "ProtocolItf.h"
+
+#include <string>
+#include <vector>
+
+class FlarmHost final : public ProtocolItf
+{
+public:
+    FlarmHost(int mp, ProtocolState &sm) : ProtocolItf(NAVI_DEV, mp, sm) {}
+    virtual ~FlarmHost() = default;
+    ProtocolType getProtocolId() override { return FLARMHOST_P; }
+
+public:
+    datalink_action_t nextByte(const char c) override;
+
+private:
+};

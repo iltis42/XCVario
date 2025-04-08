3372da470ad56e8c0c011831120ca3389e412f90
Helmut J. Rohs <helmut.j.rohs@web.de>
2025-04-08 21:44:31 +0200

    No serial baud rate "off".

--- main/comm/SerialLine.h
+++ main/comm/SerialLine.h
@@ -10,7 +10,7 @@
 
 #include <atomic>
 
-typedef enum { BAUD_OFF, BAUD_4800, BAUD_9600, BAUD_19200, BAUD_38400, BAUD_57600, BAUD_115200 } e_baud;
+typedef enum { BAUD_4800, BAUD_9600, BAUD_19200, BAUD_38400, BAUD_57600, BAUD_115200 } e_baud;
 typedef enum { SM_FLARM, SM_RADIO, SM_XCTNAV_S3, SM_OPENVARIO, SM_XCFLARMBIN, SM_XCFLARMVIEW } e_profile;
 
 typedef struct _s_serial_cfg {

01f5f49209c7b13f54743a3ef233b7560a8a9762
Helmut J. Rohs <helmut.j.rohs@web.de>
2025-08-03 12:46:32 +0200

    Cycle counting and optimization.

--- main/math/Trigenometry.h
+++ main/math/Trigenometry.h
@@ -6,7 +6,25 @@
  ***       Copyright (C) Rohs Engineering Design         ***
  ***********************************************************/
 
+ #pragma once
+ 
+// general trigenometric helper
+constexpr double My_PId = 3.14159265358979323846;
+constexpr float My_PIf = static_cast<float>(My_PId);
+constexpr float PI2f = 2.0f * static_cast<float>(My_PId);
 
+constexpr double deg2rad(double degrees) { return degrees * (My_PId / 180.0); }
+constexpr float deg2rad(float degrees)   { return degrees * (My_PIf / 180.0); }
+constexpr double rad2deg(double rad)     { return rad * 180.0 / My_PId; }
+constexpr float rad2deg(float rad)       { return rad * 180.0 / My_PIf; }
+
+// integer angle math
+int normalizeDeg(int angle);
+int angleDiffDeg(int a1, int a2);
+
+float fast_floorf(float x);
+
+// fast gauge routines with reduced precision
 float fast_sin_deg(float angle);
 float fast_cos_deg(float angle);
 float fast_sin_rad(float rad);
@@ -20,3 +38,4 @@ float fast_tan_deg(float deg);
 float fast_tan_rad(float rad);
 
 int count_digits(unsigned int n);
+

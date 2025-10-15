26826d78eea4590b647991ac3a1c7ff8633ce0f0
Eckhard Völlm <e.voellm@mail.de>
2025-10-16 01:10:39 +0200

    compiler now supports __FILENAME__ without path in recent IDF, no more recursion needed to extract

--- main/logdef.h
+++ main/logdef.h
@@ -1,23 +1,9 @@
 #pragma once
 
 #include <esp_log.h>
-#include <cstring>
 
-
-template <typename T, size_t S>
-
-inline constexpr size_t get_file_name_offset(const T (& str)[S], size_t i = S - 1)
-{
-    return (str[i] == '/' || str[i] == '\\') ? i + 1 : (i > 0 ? get_file_name_offset(str, i - 1) : 0);
-}
-
-template <typename T>
-inline constexpr size_t get_file_name_offset(T (& str)[1])
-{
-    return 0;
-}
-
-#define FNAME &__FILE__[get_file_name_offset(__FILE__)]
+// __FILENAME__ liefert nur den Dateinamen, ohne Pfad
+#define FNAME __FILENAME__
 
 #if defined(CONFIG_LOG_DISABLED)
 # include "logdefnone.h"
